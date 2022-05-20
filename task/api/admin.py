cf6cdc8962b864e20db6abefae0c2c58ed1ea691
unknown <fhaydari@armagroupholding.com>
2022-05-20 15:34:57 +0300

    admin register

--- task/api/admin.py
+++ task/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Passenger
+from .models import Trip
+
+# Register your models here.
+admin.site.register(Passenger)
+admin.site.register(Trip)
